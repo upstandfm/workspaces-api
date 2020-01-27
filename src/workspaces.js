1c23c7441bc30abcad0015405dd549b3d050ae72
Daniël Illouz <daniel.illouz@me.com>
2020-01-27 15:58:58 +0100

    Implement endpoint to fetch workspace members
    Fixes #2

--- src/workspaces.js
+++ src/workspaces.js
@@ -28,5 +28,31 @@ module.exports = {
       ProjectionExpression: 'id, createdBy, createdAt, updatedAt, #n, slug'
     };
     return client.get(params).promise();
+  },
+
+  /**
+   * Get all workspace members.
+   *
+   * @param {Object} client - DynamoDB document client
+   * @param {String} tableName
+   * @param {String} workspaceId
+   *
+   * @return {Promise} Resolves with DynamoDB data
+   *
+   * For SDK documentation see:
+   * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#query-property
+   */
+  getMembers(client, tableName, workspaceId) {
+    const params = {
+      TableName: tableName,
+      ExpressionAttributeValues: {
+        ':pk': `workspace#${workspaceId}`,
+        ':sk_start': 'member#'
+      },
+      KeyConditionExpression: 'pk = :pk and begins_with(sk, :sk_start)',
+      ProjectionExpression:
+        'id, createdBy, createdAt, updatedAt, fullName, email'
+    };
+    return client.query(params).promise();
   }
 };
